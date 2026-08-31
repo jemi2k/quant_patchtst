58d5bb38026e66d394323117be27688f65847bdb
Ermias Bekele <jermijwll@gmail.com>
2026-08-31 23:35:13 +0800

    remove unused attention factor from attention

--- layers/SelfAttention_Family.py
+++ layers/SelfAttention_Family.py
@@ -4,7 +4,7 @@ import numpy as np
 from math import sqrt
 
 class FullAttention(nn.Module):
-    def __init__(self, mask_flag=True, factor=5, scale=None, attention_dropout=0.1, output_attention=False):
+    def __init__(self, mask_flag=True, scale=None, attention_dropout=0.1, output_attention=False):
         super().__init__()
         self.scale = scale
         self.mask_flag = mask_flag
